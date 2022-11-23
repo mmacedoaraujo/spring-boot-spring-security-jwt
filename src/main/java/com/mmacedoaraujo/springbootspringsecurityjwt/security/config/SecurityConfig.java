ea7d5c5261eda89cc75358200f181b695964f42e
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-23 18:26:59 -0300

    created customauthenticationfilter

--- src/main/java/com/mmacedoaraujo/springbootspringsecurityjwt/security/config/SecurityConfig.java
+++ src/main/java/com/mmacedoaraujo/springbootspringsecurityjwt/security/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.mmacedoaraujo.springbootspringsecurityjwt.security.config;
 
+import com.mmacedoaraujo.springbootspringsecurityjwt.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,11 +12,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final AuthenticationManager authenticationManagerBean;
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
@@ -23,7 +28,10 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean));
     }
 }
