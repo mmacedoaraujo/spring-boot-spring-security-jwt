5512051d3b0807e14cefd9a5117ae38754ff49b0
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-23 16:30:09 -0300

    created AppUserController and established database connection

--- src/main/java/com/mmacedoaraujo/springbootspringsecurityjwt/service/AppUserServiceImpl.java
+++ src/main/java/com/mmacedoaraujo/springbootspringsecurityjwt/service/AppUserServiceImpl.java
@@ -23,16 +23,19 @@ public class AppUserServiceImpl implements AppUserService {
 
     @Override
     public AppUser saveUser(AppUser appUser) {
+        log.info("Saving new user {} to the database", appUser.getName());
         return appUserRepository.save(appUser);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
         return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         Role founRole = roleRepository.findByName(roleName);
         AppUser foundAppUser = getUser(username);
         foundAppUser.getRoles().add(founRole);
@@ -40,11 +43,13 @@ public class AppUserServiceImpl implements AppUserService {
 
     @Override
     public AppUser getUser(String username) {
+        log.info("Fetching user {}", username);
         return appUserRepository.findByUsername(username);
     }
 
     @Override
     public List<AppUser> getUsers() {
+        log.info("Fetching all users");
         return appUserRepository.findAll();
     }
 }
