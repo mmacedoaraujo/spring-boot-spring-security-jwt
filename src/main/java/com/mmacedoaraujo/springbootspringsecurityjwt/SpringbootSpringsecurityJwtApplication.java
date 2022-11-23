7570a2f463fd819663dd57ca5761db821abe596f
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-23 12:34:48 -0300

    start of the project

---
+++ src/main/java/com/mmacedoaraujo/springbootspringsecurityjwt/SpringbootSpringsecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.mmacedoaraujo.springbootspringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringsecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringsecurityJwtApplication.class, args);
+	}
+
+}
